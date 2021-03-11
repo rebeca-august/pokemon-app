bb0575e44e78cd4c013d0b77474d67829346ab3c
rebeca-august <rebeca.cazacu@gmail.com>
2021-03-11 20:46:10 +0000

    finish the app skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { PokemonsContainer } from './containers/PokemonsContainer'
 
 const App = () => {
-  return <h1>Hello world</h1>
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon2.vercel.app/',
+  })
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  )
 }
 
 export default App
