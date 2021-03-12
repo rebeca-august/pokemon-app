cd2d75cc7cb9308a20d3eee5013bc42955dada85
rebeca-august <rebeca.cazacu@gmail.com>
2021-03-12 22:04:48 +0000

    finish the app

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -5,7 +5,7 @@ import { GET_POKEMONS } from '../graphql/get-pokemons'
 
 export const PokemonsContainer = () => {
   const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-    variables: { first: 9 },
+    variables: { first: 20 },
   })
 
   return (
